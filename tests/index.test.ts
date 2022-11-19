b374508e9774ef40b940b2e9b14177de97ed0dec
Maurits Wilke <mauritswilke@gmail.com>
2022-11-19 19:32:00 +0100

    refactor: :recycle: moved concurrent to suite instead of individual tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,48 +7,48 @@ import { expect, it, describe, expectTypeOf } from "vitest";
 import { Book } from "../src/Book";
 import Storytel from "../src/index";
 
-describe("User", async () => {
+describe.concurrent("User", async () => {
 	if (!process.env.EMAIL || !process.env.PASSWORD) throw new Error("Make sure you have a .env in root with your EMAIL and PASSWORD");
 
 	const client = new Storytel();
 	const user = await client.signIn(process.env.EMAIL, process.env.PASSWORD);
 
-	it.concurrent("context.user should be logged in and refreshToken and singleSignToken should be set", () => {
+	it("context.user should be logged in and refreshToken and singleSignToken should be set", () => {
 		expect(user.LoginResponse.accountInfo.loginStatus).not.toEqual(-1);
 		expect(user.getSingleSignToken()).not.toBeNull();
 		expect(user.getJWT).not.toBeNull();
 	})
 
-	it.concurrent("should get the users bookshelf", async () => {
+	it("should get the users bookshelf", async () => {
 		const bookshelf = await user.getBookshelf();
 		expect(bookshelf[0]).toBeInstanceOf(Book);
 	})
 
-	it.concurrent("should get the users account info", async () => {
+	it("should get the users account info", async () => {
 		const accountInfo = await user.getAccountInfo();
 		expect(accountInfo.id).toBeDefined();
 	})
 
-	it.concurrent("should log in using user1's token", async () => {
+	it("should log in using user1's token", async () => {
 		const user2 = await client.signInUsingSingleSignToken(user.getSingleSignToken());
 		expect(user2.LoginResponse.accountInfo.loginStatus).not.toEqual(-1);
 	})
 
-	describe("Book", async () => {
+	describe.concurrent("Book", async () => {
 		const bookshelf = await user.getBookshelf();
 		const book = bookshelf[0];
 
-		it.concurrent("should return the book details", async () => {
+		it("should return the book details", async () => {
 			const details = await book.getBookDetails();
 			expect(details).toBeDefined();
 		})
 
-		it.concurrent("should return the average book rating", async () => {
+		it("should return the average book rating", async () => {
 			const averageRating = await book.getAverageRating();
 			expect(averageRating).toBeDefined();
 		})
 
-		it.concurrent("should return the epub file", async () => {
+		it("should return the epub file", async () => {
 			const ebook = await book.getEBook();
 
 			// You have to manually test if this is a valid ebook
